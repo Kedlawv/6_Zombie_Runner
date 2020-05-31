308524de37938b1c9ee6a1cbe8337bf53d4b63cd
Kedlawv <waldek.bus@gmail.com>
2020-05-31 14:47:38 +0200

    Player Flashlight | Flashlight script -> intensity decay over time | Environment lights |

---
+++ Assets/Scripts/FlashlightSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreseLightIntensity();
+    }
+
+    private void DecreseLightIntensity()
+    {
+        if (myLight.intensity >= 0.5f)
+        {
+            myLight.intensity -= lightDecay * Time.deltaTime;
+        }
+    }
+}
